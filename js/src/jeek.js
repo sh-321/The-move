0180bf880d8c94b3feb43ff8700d04e7c023afe6
sh-321 <tmdghksrhtn@naver.com>
2020-12-24 19:40:16 +0900

    asd

---
+++ js/src/jeek.js
@@ -0,0 +1,55 @@
+(function($){
+    // 헤더 따라내려오는 기능
+    
+    // var header=$('.nav_pc');
+    // var headoffset1=header.offset().top;
+    // console.log(headoffset1);
+    
+    // $(window).on('scroll',function(e){
+    //     e.preventDefault();
+    //     var scroll=$(this).scrollTop();
+        
+    //     if(headoffset1<=scroll){
+    //         header.css({position:'fixed', margin:'auto', left:'50%', marginLeft:'-500px', top:0,zIndex:2000,bottom:'auto',opacity:0.8});}else
+    //         {header.css({position:'static',margin:'auto',opacity:1});}
+        
+      
+    
+    //     console.log(scroll);
+    //     console.log(headoffset1);
+    
+    // });
+    
+ // 포트폴리오 호버시 드롭다운 잘못됨
+    // var h = $('.hover');
+    // var c = $('.category');
+
+
+    // h.on('mouseover',function() {
+    //     c.css({display:'block'})
+        
+    // })
+
+    // c.on('mouseover',function() {
+    //     c.css({display:'block'})
+        
+    // })
+    
+    
+    // h.on('mouseleave',function (){
+    //     c.css({display: 'none'})
+    // })
+    
+    
+       
+    
+        
+    
+    
+    
+    
+    
+    })(jQuery);
+    
+    
+   
